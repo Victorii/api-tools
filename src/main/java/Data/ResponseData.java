79a8c1a6bbc90f2bd478b2752b5ea5d4ddd4eb9d
victoria.maziar <E5Jjvu`2F>
2019-11-21 18:09:39 +0200

    add ResponseData class

---
+++ src/main/java/Data/ResponseData.java
@@ -0,0 +1,98 @@
+package Data;
+
+import io.restassured.http.Cookies;
+import io.restassured.http.Headers;
+import io.restassured.response.ResponseBody;
+
+public class ResponseData {
+    /**
+     * private constructor to avoid multiple object instances.
+     * access only from public getData() method
+     */
+    private ResponseData() {
+        responseBody = null; //TODO: NPE?
+        responseHeaders = new Headers();
+        responseCookies = new Cookies();
+        statusCode = 0;
+    }
+
+    /**
+     * @return instance of ResponseData object due to single object existence
+     */
+    public static ResponseData getData(){
+        return ResponseDataHolder.instance;
+    }
+
+    /**
+     * private inner class intended to store constant of instance
+     * it provides possibility to work with only single object of ResponseData
+     */
+    private static class ResponseDataHolder {
+        private static final ResponseData instance = new ResponseData();
+    }
+
+    private ResponseBody responseBody;
+    private Headers responseHeaders;
+    private Cookies responseCookies;
+    private int statusCode;
+
+    /**
+     * @return response cookie
+     */
+    public Cookies getResponseCookies() {
+        return responseCookies;
+    }
+
+    /**
+     * @return response headers
+     */
+    public Headers getResponseHeaders() {
+        return responseHeaders;
+    }
+
+    /**
+     * @return response body
+     */
+    public ResponseBody getResponseBody() {
+        return responseBody;
+    }
+
+    /**
+     * @return status code
+     */
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    /**
+     * set value to responseBody for single object
+     * @param responseBody
+     */
+    protected void setResponseBody(ResponseBody responseBody) {
+        this.responseBody = responseBody;
+    }
+
+    /**
+     * set value to responseCookies for single object
+     * @param responseCookies
+     */
+    protected void setResponseCookies(Cookies responseCookies) {
+        this.responseCookies = responseCookies;
+    }
+
+    /**
+     * set value to responseHeaders for single object
+     * @param responseHeaders
+     */
+    protected void setResponseHeaders(Headers responseHeaders) {
+        this.responseHeaders = responseHeaders;
+    }
+
+    /**
+     * set value to statusCode for single object
+     * @param statusCode
+     */
+    protected void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+}
