f6d69c35aedc9fb762346f26b165dab4baccfe80
victoria.maziar <E5Jjvu`2F>
2019-11-22 16:16:30 +0200

    WIP

--- src/test/java/TestRestAssured.java
+++ src/test/java/TestRestAssured.java
@@ -1,5 +1,10 @@
+import RestAssured.RestAssuredResponseData;
 import RestAssured.RestAssuredWrapper;
 import org.testng.annotations.Test;
+
+import javax.json.Json;
+import javax.json.stream.JsonParser;
+
 
 public class TestRestAssured {
 
@@ -9,5 +14,8 @@ public class TestRestAssured {
         RestAssuredWrapper restAssuredWrapper = new RestAssuredWrapper();
         restAssuredWrapper.setURL("https://api.github.com/repositories");
         restAssuredWrapper.getResponse();
+
+        JsonParser object = Json.createParser(RestAssuredResponseData.getData().getResponseBody().asInputStream());
+
     }
 }
